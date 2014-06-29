1f96b19ffbb0a1bd49fd0578a42c5490423902fb
Omid Omid <omidbachari@gmail.com>
2014-06-29 07:37:48 -0400

    Tests completed

--- spec/features/manufacturer_spec.rb
+++ spec/features/manufacturer_spec.rb
@@ -12,24 +12,72 @@ feature '', %Q(
 # Form validations prevent invalid submissions, and acknowledge success when properly submitted.
 # Required fields are name and country.
 
-  # scenario 'visit car index and see list of manufacturers' do
+  scenario 'visit car index and see list of manufacturers' do
+    attrs = {
+      name: 'Mercedes Benz',
+      country: 'Germany'
+    }
 
-  # end
+    manufacturer = Manufacturer.create!(attrs)
 
-  # scenario 'click on add a manufacturer link and form works' do
+    visit manufacturers_path
 
-  # end
+    expect(page).to have_content manufacturer.name
+    expect(page).to have_content manufacturer.country
+  end
 
-  # scenario 'manufacturer form, name missing' do
+  scenario 'click on add a manufacturer link and form works' do
+    attrs = {
+      name: 'Mercedes Benz',
+      country: 'Germany'
+    }
 
-  # end
+    manufacturer = Manufacturer.create!(attrs)
 
-  # scenario 'manufacturer form, country missing' do
+    visit manufacturers_path
 
-  # end
+    expect(page).to have_content manufacturer.name
+    expect(page).to have_content manufacturer.country
+    click_on 'Add manufacturer.'
+    fill_in 'Name', with: 'Aston Martin'
+    fill_in 'Country', with: 'Great Britain'
+    click_button 'Submit'
+    expect(page).to have_content 'Aston Martin'
+    expect(page).to have_content 'Great Britain'
+    expect(page).to have_content 'You have successfully added a manufacturer'
+  end
 
-  # scenario 'manufacturer appears in car form dropdown' do
+  scenario 'manufacturer form, name missing' do
 
-  # end
+    visit new_manufacturer_path
+
+    fill_in 'Country', with: 'Great Britain'
+    click_button 'Submit'
+    expect(page).to have_content 'Something went wrong'
+    expect(page).to have_content 'Name can\'t be blank'
+  end
+
+  scenario 'manufacturer form, country missing' do
+
+    visit new_manufacturer_path
+
+    fill_in 'Name', with: 'Aston Martin'
+    click_button 'Submit'
+    expect(page).to have_content 'Something went wrong'
+    expect(page).to have_content 'Country can\'t be blank'
+  end
+
+  scenario 'manufacturer appears in car form dropdown' do
+    attrs = {
+      name: 'Mercedes Benz',
+      country: 'Germany'
+    }
+
+    manufacturer = Manufacturer.create!(attrs)
+
+    visit new_car_path
+
+    expect(page).to have_content 'Mercedes Benz'
+  end
 end
 
