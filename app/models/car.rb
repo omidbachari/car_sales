ef223db6d978e76d6cb7dd581ba03a51c9f9eed5
Omid Omid <omidbachari@gmail.com>
2014-06-30 10:00:28 -0400

    Precense validated-  year

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,6 +6,7 @@ class Car < ActiveRecord::Base
   validates :mileage, presence: true
   validates_numericality_of :mileage, :greater_than_or_equal_to => 0
   validates :model_name, presence: true
+  validates :year, presence: true
   validates_numericality_of :year, :greater_than_or_equal_to => 1920
   validates_numericality_of :manufacturer_id
 end
